fa7e362179268a5807c9f89f387268f04e8f619b
Irelandoracle1 <100025415+Irelandoracle1@users.noreply.github.com>
2025-08-18 23:37:11 +0100

    setting up for heroku

--- naijarestaurant/settings.py
+++ naijarestaurant/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i*oe=!j=eiflpy5l5ox%aj5h%q(l(xinvn*)&rqq8wmqv1*!$)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
