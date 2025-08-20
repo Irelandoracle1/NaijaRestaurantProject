ea9ea3a3921cad36fa4a9e0b170e8eb7e13a1564
Irelandoracle1 <100025415+Irelandoracle1@users.noreply.github.com>
2025-08-20 23:22:06 +0100

    created custom models and registered them to the admin area

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Menu, Booking
+
 
 # Register your models here.
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+    search_fields = ("name",)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("user", "date", "time", "guests", "menu", "created_at")
+    list_filter = ("date", "time", "menu")
+    search_fields = ("user__username", "user__email")
