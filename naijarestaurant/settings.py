73c58eb9f3e83e2efa8b711d55f592b26852d152
Irelandoracle1 <100025415+Irelandoracle1@users.noreply.github.com>
2025-08-25 23:29:25 +0100

    restored secret  key to  fix heroku error

--- naijarestaurant/settings.py
+++ naijarestaurant/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY  = os.environ.get("SPECIAL_KEY")
-
+SECRET_KEY = 'django-insecure-i*oe=!j=eiflpy5l5ox%aj5h%q(l(xinvn*)&rqq8wmqv1*!$)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -123,7 +122,6 @@ WSGI_APPLICATION = 'naijarestaurant.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 
 # DATABASES = {
